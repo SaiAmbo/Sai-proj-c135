14415990f2ed9d8b71fc7f2290fbc2ccd04ff9b2
SaiAmbo <90088973+SaiAmbo@users.noreply.github.com>
2022-08-14 10:21:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+video = "";
+
+function preload(){
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+function setup(){
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+}
+
